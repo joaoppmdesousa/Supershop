7fc922bacceff25865fc18772e3ce955c5e1edfc
joaoppmdesousa <joaopedropsousa@gmail.com>
2025-04-26 18:02:50 +0100

    Modify Products.

--- Supershop/Data/Entities/Product.cs
+++ Supershop/Data/Entities/Product.cs
@@ -7,6 +7,8 @@ namespace Supershop.Data.Entities
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
 
@@ -19,11 +21,11 @@ namespace Supershop.Data.Entities
 
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
 
         [Display(Name = "Is Available")]
